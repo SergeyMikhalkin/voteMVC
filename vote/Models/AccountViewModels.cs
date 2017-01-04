91bbf8c0f0a56637789f5afe1efe287405b6c827
SergeyMikhalkin <Sergey.Mikhalkin.work@gmail.com>
2017-01-04 23:13:04 +0300

    Edited: - register view and model - DB connection string

--- vote/Models/AccountViewModels.cs
+++ vote/Models/AccountViewModels.cs
@@ -72,13 +72,26 @@ namespace vote.Models
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Повторите пароль")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Имя")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Дата рождения")]
+        [DataType(DataType.Date)]
+        public string DateOfBorn { get; set; }
     }
 
     public class ResetPasswordViewModel
