dbba0b2ca60edb57a5c56e40d4d16b3f5c905730
SergeyMikhalkin <Sergey.Mikhalkin.work@gmail.com>
2017-01-08 23:58:41 +0300

    Added competition controller and view

---
+++ vote/Controllers/CompetitionsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vote.Controllers
+{
+    public class CompetitionsController : Controller
+    {
+        // GET: Competitions
+        public ActionResult AllCompetitions()
+        {
+            return View();
+        }
+    }
+}
