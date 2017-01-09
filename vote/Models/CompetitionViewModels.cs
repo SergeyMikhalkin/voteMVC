a1d5ca0f146d979d68b41dce6a5b9f6a16ff987b
SergeyMikhalkin <Sergey.Mikhalkin.work@gmail.com>
2017-01-09 22:48:54 +0300

    Added CompetitionViewModel

---
+++ vote/Models/CompetitionViewModels.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace vote.Models
+{
+    public class AdditionViewModel
+    {
+        [Required]
+        [Display(Name = "Название мероприятия")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Текст")]
+        public string Text { get; set; }
+
+        [Required]
+        [Display(Name = "Дата проведения")]
+        [DataType(DataType.Date)]
+        public string Date { get; set; }
+
+        [Required]
+        [Display(Name = "Место")]
+        public string Place { get; set; }
+
+        [Required]
+        [Display(Name = "Вид соревнования")]
+        public string Kind { get; set; }
+
+    }
+
+}
