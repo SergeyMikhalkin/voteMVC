d6936de0a951ec213a60d33921535aadec3990e7
SergeyMikhalkin <Sergey.Mikhalkin.work@gmail.com>
2017-01-11 22:20:27 +0300

    Fixed bug in user registration.

--- vote/Controllers/AccountController.cs
+++ vote/Controllers/AccountController.cs
@@ -151,7 +151,15 @@ namespace vote.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    DateOfBorn = model.DateOfBorn
+                };
+
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
