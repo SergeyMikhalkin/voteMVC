35801a983918d9019b32e786b26825a56e517406
SergeyMikhalkin <Sergey.Mikhalkin.work@gmail.com>
2017-02-07 23:12:29 +0300

    Added Group model.

---
+++ vote/Models/Group.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vote.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Competition> Competitions { get; set; }
+
+        public Group()
+        {
+            Competitions = new List<Competition>();
+        }
+    }
+}
