69cbfde855e05ebb1cb9af282c7933ef35c3b3f4
SergeyMikhalkin <Sergey.Mikhalkin.work@gmail.com>
2017-02-11 23:30:46 +0300

    Added Rating model.

---
+++ vote/Models/Rating.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vote.Models
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Competition> Competitions { get; set; }
+
+        public Rating()
+        {
+            Competitions = new List<Competition>();
+        }
+    }
+}
