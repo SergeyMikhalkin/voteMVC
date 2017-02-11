9e783c86581faf88729a1347d54d8692c0192ada
SergeyMikhalkin <Sergey.Mikhalkin.work@gmail.com>
2017-02-11 23:28:27 +0300

    Added Comment model.

---
+++ vote/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vote.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string FieldName { get; set; }
+        public string Text { get; set; }
+        public int CompetitionId { get; set; }
+        public Competition Competition { get; set; }
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
