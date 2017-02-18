4db4186481621250b234f0264522f839f9f74076
SergeyMikhalkin <Sergey.Mikhalkin.work@gmail.com>
2017-02-18 18:56:47 +0300

    User can't vote twice in Competitions(All) view.

--- vote/Controllers/CompetitionsController.cs
+++ vote/Controllers/CompetitionsController.cs
@@ -16,26 +16,57 @@ namespace vote.Controllers
         // GET: Competitions
         public ActionResult All()
         {
+            string userName = User.Identity.Name;
+            if(userName != string.Empty)
+            {
+                string userId = string.Empty;
+                try
+                {
+                    userId = db.Users.Single(x => x.UserName == userName).Id;
+                }
+                catch (Exception)
+                {
+                    return View("Error");
+                }
+
+                Hashtable votedCompetitions = new Hashtable();
+                FillVotedCompetitions(votedCompetitions, userId);
+                ViewBag.VotedCompetitions = votedCompetitions;
+            }
+            
             Hashtable CountOfVoters = new Hashtable();
             FillCountOfVoters(CountOfVoters);
             ViewBag.CountOfVoters = CountOfVoters;
+
             ViewBag.CompetitionCount = db.Competitions.Count();
             ViewBag.Competition = db.Competitions.ToArray();
 
             return View();
         }
 
+        // Voted competitions user's hashtable
+        private void FillVotedCompetitions(Hashtable votedCompetitions, string id)
+        {
+            var _votedCompetitons = from c in db.Votes
+                                    where c.UserId == id
+                                    select c;
+            foreach (var competition in _votedCompetitons)
+            {
+                votedCompetitions.Add(competition.Id, true);
+            }
+                                    
+        }
+
         // fill hashtable 'competition' -> 'count of voters'
         private void FillCountOfVoters(Hashtable countOfVoters)
         {
             var votersGroupByMapID = from v in db.Votes
-                                     group v by v.MapID;
+                                     group v by v.CompetitionID;
 
             foreach (var group in votersGroupByMapID)
             {
                 countOfVoters.Add(group.Key, group.Count());
             }
         }
-
     }
 }
