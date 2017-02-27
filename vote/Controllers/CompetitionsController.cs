02bcd2c4984044d5b724939e40dce7aaadd3e8a0
SergeyMikhalkin <Sergey.Mikhalkin.work@gmail.com>
2017-02-27 22:13:09 +0300

    Changed distance part in voting(view and action).

--- vote/Controllers/CompetitionsController.cs
+++ vote/Controllers/CompetitionsController.cs
@@ -343,6 +343,47 @@ namespace vote.Controllers
             return View("Distance", voteViewModel);
         }
 
+        [HttpPost]
+        public ActionResult Distance(int distanceGrade, string commentAboutDistance, VoteViewModel voteViewModel)
+        {
+            if (voteViewModel == null) return View("Error");
+
+            if (commentAboutDistance != string.Empty)
+            {
+
+                // create comment
+                Comment newComment = new Comment() { Text = commentAboutDistance, FieldName = "Distance" };
+
+                // get user
+                ApplicationUser user = new ApplicationUser();
+                if (getUserByName(User.Identity.Name, ref user) == "Error")
+                {
+                    return View("Error");
+                }
+
+                Competition competition = new Competition();
+                if (getCompetitionById(voteViewModel.CompetitionID, ref competition) == "Error")
+                {
+                    return View("Error");
+                }
+
+                // assign user and competition to comment
+                newComment.User = user;
+                newComment.Competition = competition;
+                newComment.UserId = user.Id;
+                newComment.CompetitionId = competition.Id;
+
+                // create new comment in db
+                db.Comments.Add(newComment);
+                db.SaveChanges();
+            }
+
+            voteViewModel.Distance = distanceGrade;
+            ModelState.Clear();
+
+            return View("Start", voteViewModel);
+        }
+
         private string getCompetitionById(int competitionId, ref Competition competition)
         {
             try
