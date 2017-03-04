0a884e13910efead113e18f9152816548fa5fa34
SergeyMikhalkin <Sergey.Mikhalkin.work@gmail.com>
2017-03-04 23:57:31 +0300

    Added comments view.

---
+++ vote/Controllers/CommentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vote.Models;
+
+namespace vote.Controllers
+{
+    public class CommentController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Comments for map
+        public ActionResult Show(int id)
+        {
+            // Info about competition
+            ViewBag.Competition = db.Competitions.Single(competition => competition.Id == id);
+
+            // Get comments
+            var comments = db.Comments.Where(comment => comment.CompetitionId == id).GroupBy(field => field.FieldName);
+
+            return View(comments);
+        }
+    }
+}
