475a1cd2dc1eef593d5f186cadde2063590614b6
SergeyMikhalkin <Sergey.Mikhalkin.work@gmail.com>
2017-03-15 23:16:10 +0100

    Added admin controller. Admin may to look the competition list.

---
+++ vote/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vote.Models;
+
+namespace vote.Controllers
+{
+    public class AdminController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin
+        [Authorize(Roles ="Admin")]
+        public ActionResult Competitions()
+        {
+            var competitions = db.Competitions;
+            return View(competitions);
+        }
+    }
+}
