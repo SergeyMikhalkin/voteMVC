208b3099c25d5967803fc7a979f8ef8b6e11cf73
SergeyMikhalkin <Sergey.Mikhalkin.work@gmail.com>
2017-03-24 22:57:47 +0100

    Changed Index page.

--- vote/App_Start/RouteConfig.cs
+++ vote/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace vote
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Competitions", action = "All", id = UrlParameter.Optional }
             );
         }
     }
